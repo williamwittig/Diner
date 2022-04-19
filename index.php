4fc42ef97a9301f623657b89e12cd88762c8cf2b
williamwittig2 <wwittig@student.greenriver.edu>
2022-04-19 13:37:34 -0400

    Updated index page

--- index.php
+++ index.php
@@ -10,9 +10,22 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 // Define a default route
+// Home page rendering
 $f3->route('GET /', function() {
     $view = new Template();
-    echo "<h1>My Diner</h1>";
+    echo $view->render('views/home.html');
+});
+
+// Breakfast page rendering
+$f3->route('GET /breakfast', function() {
+    $view = new Template();
+    echo $view->render('views/breakfast.html');
+});
+
+// Lunch page rendering
+$f3->route('GET /lunch', function() {
+    $view = new Template();
+    echo $view->render('views/lunch.html');
 });
 
 // Run fat free
